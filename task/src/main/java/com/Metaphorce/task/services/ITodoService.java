ee96076f3ac11fa4dcdaa759ab22dd407e4b6db6
Saúl Landa <landagarcia.sauleduardo@gmail.com>
2024-07-13 11:15:48 -0600

    Add service

--- task/src/main/java/com/Metaphorce/task/services/ITodoService.java
+++ task/src/main/java/com/Metaphorce/task/services/ITodoService.java
@@ -1,4 +1,13 @@
 package com.Metaphorce.task.services;
 
+import com.Metaphorce.task.models.Todo;
+
+import java.util.List;
+
 public interface ITodoService {
+    List<Todo> findAll();
+    List<Todo> findAllCompleted();
+    Todo save(Todo todo);
+    Todo update(Todo todo);
+    void delete(Long id);
 }
